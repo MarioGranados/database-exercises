ddc11b38b5ef4c0bdb30ebaa317a895f09903c1f
Mario Granados <mario.alberto.granados1@gmail.com>
2022-03-17 15:51:49 -0500

    seeder file created

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,10 +6,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_name VARCHAR(10) DEFAULT 'NONE',
-    record_name VARCHAR(20) NOT NULL,
+    artist_name VARCHAR(20) DEFAULT 'NONE',
+    record_name VARCHAR(50) NOT NULL,
     release_date INT NOT NULL,
     sales FLOAT NOT NULL,
-    genre VARCHAR(10) NOT NULL,
+    genre VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
 );

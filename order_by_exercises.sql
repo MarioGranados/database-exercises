232203292c859f01abb722c0c43fc8878ac903ad
Mario Granados <mario.alberto.granados1@gmail.com>
2022-03-21 11:04:57 -0500

    finished

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,8 +1,18 @@
 USE employees;
 
-SELECT first_name FROM employees WHERE first_name IN ('Irene', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
+
+
+
 SELECT last_name FROM employees WHERE last_name LIKE 'E%';
 SELECT last_name FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no DESC;
+
+
 
 SELECT first_name FROM employees WHERE last_name IN ('Irene', 'Vidya') or last_name = 'Maya';
 SELECT first_name FROM employees WHERE first_name IN ('Irene', 'Vidya', 'Maya') AND gender = 'M';
